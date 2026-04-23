8043550e6ed64bed30032ca2d20406e044bdedc2
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-04-23 17:21:57 -0300

    feat: finish the first class

---
+++ Bro Code classes/Class1.java
@@ -0,0 +1,15 @@
+public class Class1 {
+    public static void main(String[] args){
+
+        // This is a comment.
+
+        /*
+            And this is a 
+            multi-line 
+            comment!
+        */
+
+        System.out.print("Hello world!\n");
+        System.out.println("This is a second line!");
+    }
+}
