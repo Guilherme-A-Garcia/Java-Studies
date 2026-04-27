906902b3fa1f5543524bfff8cc7ab6116aeeb6e7
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-04-27 20:28:13 -0300

    feat: finish the fifth class

---
+++ Bro Code classes/Class5.java
@@ -0,0 +1,48 @@
+// Arithmetic - Operators
+
+public class Class5 {
+    public static void main(String[] args) {
+        
+        // Basic Operations
+        // int x = 10;
+        // int y = 2;
+        // int z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        // z = x % y; The % modulus operator gives the remainder of a division.
+        
+        // Augmented Assignment Operators
+        // int x = 10;
+        // int y = 3;
+
+        // x = x + y would technically work, but a simpler way is the following:
+        // x += y;
+        // x -= y;
+        // x *= y;
+        // x /= y; Pay attention to data types if you need decimals!
+        // x %= y;
+
+        // Increment and Decrement Operators
+        /* Instead of x = x + 1 or x += 1, just do:
+        x++;
+        x--; 
+        */
+
+        // Order of Operations (P-E-M-D-A-S)
+        /*
+        P (Parentheses) - Please
+        E (Exponents) - Excuse
+        M (Multiplication) - My
+        D (Division) - Dope
+        A (Addition) - Ass
+        S (Subtraction) - Swag
+        */
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+    }
+}
