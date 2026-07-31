5690be0c024b29611ccfd7147d8ab0304fc3ab53
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-07-30 22:29:31 -0300

    feat: finish the sixth JavaFX class

---
+++ Bro Code courses/JavaFX Course/JFX_Class6/JFXClass6.java
@@ -0,0 +1,26 @@
+// GUI Flow Management - Switching scenes
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JFXClass6 extends Application {
+    @Override
+    public void start(Stage stage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("JFXClass6Scene1.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
