bb82939564675a5fc336cc3902c45c8014da55b4
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-08-05 22:35:21 -0300

    feat: finish the eleventh JavaFX class

---
+++ Bro Code courses/JavaFX Course/JFX_Class11/JFXClass11Controller.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+public class JFXClass11Controller {
+    @FXML
+    private CheckBox myCheckBox;
+    @FXML
+    private Label myLabel;
+
+    public void change(ActionEvent event){
+        if(myCheckBox.isSelected()){
+            myLabel.setText("ON");
+        }
+        else{
+            myLabel.setText("OFF");
+        }
+    }
+}
