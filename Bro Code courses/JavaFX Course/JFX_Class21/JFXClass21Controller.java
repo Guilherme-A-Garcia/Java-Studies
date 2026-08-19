c67d9bbd0781faafb5c9d83e2845139e9542a5a6
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-08-19 20:25:18 -0300

    feat: finish the twenty-first JavaFX class

---
+++ Bro Code courses/JavaFX Course/JFX_Class21/JFXClass21Controller.java
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+
+public class JFXClass21Controller {
+    @FXML
+    MenuBar menuBar;
+    
+    public void newMethod(){
+        System.out.println("Menu 1 item selected.");
+    }
+}
